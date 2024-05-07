e24a7fe13ed3ab37631403792e3b18ee422585b6
aaronnam79 <aaronnam7901@gmail.com>
2024-05-06 18:07:51 -0700

    route fix

--- src/App.jsx
+++ src/App.jsx
@@ -10,73 +10,84 @@ import Settings from "./Settings";
 import Login from './Login';
 import Game from './Game';
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      socket: null,
-      table: [],
-      blackStarts: true,
-      whoPlay: "black",
-      tileSelected: null,
-      blackPieces: 12,
-      whitePieces: 12,
-      nexMoveInAnyDirection: false,
-      showMenu: false,
-      isAuthenticated: false // Assume authentication state can be derived from somewhere
-    };
-  }
-
-  componentDidMount() {
-    this.initSocket();
-    appActions.init(this);
-  }
-
-  initSocket = () => {
-    const socket = io('http://localhost:3000');
-    socket.on('game update', (move) => {
-      console.log("Received game update", move);
-      // Here you would handle updating the game state based on the move received
-    });
-    this.setState({ socket });
-  };
-
-  renderGame = () => {
-    const { blackPieces, whitePieces, table, whoPlay } = this.state;
-    return (
-      <div className="app">
-        <PiecesCounterBar blackPieces={blackPieces} whitePieces={whitePieces} />
-        <div className="bar">
-          <button onClick={() => appActions.showMenu(this, true)}>
-            Menu
-          </button>
-          <div className="whoPlay">{whoPlay} plays</div>
-        </div>
-        <Table table={table} onClick={(evt) => appActions.onClick(this, evt)} />
-        <div className="buttons">
-          <button onClick={() => appActions.restart(this)}>
-            Restart Game
-          </button>
-        </div>
-        {this.state.showMenu && <Settings component={this} />}
-      </div>
-    );
-  };
-
-  render() {
-    return (
-      <Router>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/game" element={this.renderGame()} />
-        </Routes>
-      </Router>
-    );
-  }
+export default function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/game" element={<Game />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App;
+// class App extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       socket: null,
+//       table: [],
+//       blackStarts: true,
+//       whoPlay: "black",
+//       tileSelected: null,
+//       blackPieces: 12,
+//       whitePieces: 12,
+//       nexMoveInAnyDirection: false,
+//       showMenu: false,
+//       isAuthenticated: false // Assume authentication state can be derived from somewhere
+//     };
+//   }
+
+//   componentDidMount() {
+//     this.initSocket();
+//     appActions.init(this);
+//   }
+
+//   initSocket = () => {
+//     const socket = io('http://localhost:3000');
+//     socket.on('game update', (move) => {
+//       console.log("Received game update", move);
+//       // Here you would handle updating the game state based on the move received
+//     });
+//     this.setState({ socket });
+//   };
+
+//   renderGame = () => {
+//     const { blackPieces, whitePieces, table, whoPlay } = this.state;
+//     return (
+//       <div className="app">
+//         <PiecesCounterBar blackPieces={blackPieces} whitePieces={whitePieces} />
+//         <div className="bar">
+//           <button onClick={() => appActions.showMenu(this, true)}>
+//             Menu
+//           </button>
+//           <div className="whoPlay">{whoPlay} plays</div>
+//         </div>
+//         <Table table={table} onClick={(evt) => appActions.onClick(this, evt)} />
+//         <div className="buttons">
+//           <button onClick={() => appActions.restart(this)}>
+//             Restart Game
+//           </button>
+//         </div>
+//         {this.state.showMenu && <Settings component={this} />}
+//       </div>
+//     );
+//   };
+
+//   render() {
+//     return (
+//       <Router>
+//         <Routes>
+//           <Route path="/" element={<Home />} />
+//           <Route path="/login" element={<Login />} />
+//           <Route path="/game" element={this.renderGame()} />
+//         </Routes>
+//       </Router>
+//     );
+//   }
+// }
+
+// export default App;
 
 
 
